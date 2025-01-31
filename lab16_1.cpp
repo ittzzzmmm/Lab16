7a1937eeb3fb4833caafd5040ca6fd6d6ef743f2
ittzzzmmm <ittzzzmmm@gmail.com>
2025-01-31 21:58:59 +0700

    ittzm

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i) ;
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *d,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(d+i) = (rand()%100 +1)/100.00;
+    }
+}
+
+void findRowSum(const double *d,double *sum1,int N,int M){
+    for(int i = 0; i<N;i++){
+        for(int j = 0; j<M ; j++){
+            *(sum1+i) += *(d+(i*M)+j) ;    
+        }
+    }
+}
+
+void findColSum(const double *d,double *sum2,int N,int M){
+	for(int i = 0; i<N;i++){
+        for(int j = 0; j<M ; j++){
+            *(sum2+j) += *(d+(i*M)+j) ;    
+        }
+    }
+}
